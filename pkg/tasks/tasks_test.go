be7bde82bccdc7a5c7822b3f58e9c51dfcb3a2a4
Pior Bastida <pior@pbastida.net>
2017-12-17 18:46:00 -0500

    Add tests on the package tasks

---
+++ pkg/tasks/tasks_test.go
@@ -0,0 +1,44 @@
+package tasks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+)
+
+func loadTestData(t *testing.T, data string) interface{} {
+	var tmp interface{}
+	err := yaml.Unmarshal([]byte(data), &tmp)
+	if err != nil {
+		t.Fatalf("Failed to load a test fixture: %s", err)
+	}
+	return tmp
+}
+
+func TestCustom(t *testing.T) {
+	data := loadTestData(t, `
+custom:
+  met?: test-command
+  meet: custom-command
+`)
+
+	task, err := BuildFromDefinition(data)
+	require.NoError(t, err, "BuildFromDefinition() failed")
+
+	require.Equal(t, task.(*Custom).command, "custom-command")
+	require.Equal(t, task.(*Custom).condition, "test-command")
+}
+
+func TestPip(t *testing.T) {
+	data := loadTestData(t, `
+pip:
+  - file1
+  - file2
+`)
+
+	task, err := BuildFromDefinition(data)
+	require.NoError(t, err, "BuildFromDefinition() failed")
+
+	require.Equal(t, task.(*Pip).files, []string{"file1", "file2"})
+}
