6a76a72b471afa1ef2f1198120f87f2e209fc7aa
Pior Bastida <pior@pbastida.net>
2017-12-17 18:42:12 -0500

    Fail on invalid project manifest when looking for the active project

--- pkg/project/current.go
+++ pkg/project/current.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/pior/dad/pkg/manifest"
@@ -13,8 +14,12 @@ var ManifestFilename = "dev.yml"
 
 func FindCurrent(path string) (*Project, error) {
 	for i := 0; i < maxDirLevel; i++ {
-		man, err := manifest.Load(filepath.Join(path, ManifestFilename))
-		if err == nil {
+		manifestPath := filepath.Join(path, ManifestFilename)
+		if exists(manifestPath) {
+			man, err := manifest.Load(manifestPath)
+			if err != nil {
+				return nil, err
+			}
 			return &Project{Path: path, Manifest: man}, nil
 		}
 
@@ -25,4 +30,20 @@ func FindCurrent(path string) (*Project, error) {
 	}
 
 	return nil, fmt.Errorf("project not found (no manifest)")
+}
+
+// exists checks if a file or directory exists.
+func exists(path string) bool {
+	if path == "" {
+		return false
+	}
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if !os.IsNotExist(err) {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return false
 }
