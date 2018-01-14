49487c0434c474ff86ad5c35db8353ebd191359d
Pior Bastida <pior@pbastida.net>
2018-01-14 09:53:35 -0500

    Fail silently in shell hook if dad executable is not reachable

--- pkg/integration/bash.go
+++ pkg/integration/bash.go
@@ -36,6 +36,10 @@ dad() {
 __dad_prompt_command() {
     # In shell hook mode, the command will use stderr to print in the console
     # and stdout to mutate the shell (like activating a Python virtualenv)
+
+    # Fail fast if no dad executable is reachable
+    which dad > /dev/null || return
+
     local hook_eval
     hook_eval="$(dad --shell-hook)"
     [ -n "${DAD_DEBUG}" ] && echo -e "DAD_DEBUG: Hook eval:\n${hook_eval}\n---"
