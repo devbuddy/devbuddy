369253eaeabd5b874c69ee93d4e6cc5a37ab00e5
Pior Bastida <pior@pbastida.net>
2018-04-14 18:21:55 -0400

    Stop at the first error when running tasks

--- pkg/tasks/runner.go
+++ pkg/tasks/runner.go
@@ -26,7 +26,7 @@ func RunAll(cfg *config.Config, proj *project.Project, ui *termui.UI) error {
 	for _, task := range taskList {
 		err = task.Perform(&ctx)
 		if err != nil {
-			break
+			return err
 		}
 	}
 
