8e64b0d03f1af1cb2e987778ee9f28dc4961cd22
Pior Bastida <pior@pbastida.net>
2018-04-22 20:04:06 -0400

    Add a http util to download a file

---
+++ pkg/utils/http.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("failed to download (code %d)", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
