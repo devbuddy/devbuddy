27bc8045f7301ad9515ff1ad1545ab79f250a203
Pior Bastida <pior@pbastida.net>
2018-04-22 20:05:07 -0400

    Better error handling in the go task

---
+++ tests/up_go_command_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -u
+
+oneTimeSetUp() {
+    eval "$(dad --shell-init)"
+}
+
+setUp() {
+    cd $SHUNIT_TMPDIR
+
+    cat > dev.yml <<YAML
+up:
+  - go: '1.5'
+YAML
+
+    rm -rf gopath
+    export GOPATH="$PWD/gopath"
+}
+
+testMissingGOPATH() {
+    unset GOPATH
+
+    output=$(dad up | grep -q 'Warning.*GOPATH')  # Expect a warning about GOPATH
+    rc=$?
+    assertEquals "dad up should warn about GOPATH not set" 0 $rc
+}
+
+testEnv() {
+    output=$(dad up)
+    rc=$?
+    assertEquals "dad command returns zero" 0 $rc
+
+    eval "$(command dad --shell-hook)"  # Simulate a prompt hook
+
+    version=$(go version | cut -d ' ' -f 3)
+    assertEquals "go version" "go1.5" "$version"
+}
+
+SHUNIT_COLOR='none'  # Not macos compatible?
+. shunit2/shunit2
