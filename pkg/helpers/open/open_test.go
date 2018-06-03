c77245f792323cf31d1b5a61f8846272a77e931f
Pior Bastida <pior@pbastida.net>
2018-06-03 18:16:53 -0400

    debug

--- pkg/helpers/open/open_test.go
+++ pkg/helpers/open/open_test.go
@@ -1,6 +1,7 @@
 package open
 
 import (
+	"os"
 	"os/exec"
 	"testing"
 
@@ -43,7 +44,14 @@ func setupProject(t *testing.T, path string) *project.Project {
 
 	cmd = exec.Command("git", "commit", "-m", "Commit1", "--allow-empty")
 	cmd.Dir = path
-	cmd.Env = []string{"NAME=John", "EMAIL=john@doo.com"}
+	cmd.Env = []string{
+		"GIT_COMMITTER_NAME=John",
+		"GIT_AUTHOR_NAME=John",
+		"GIT_COMMITTER_EMAIL=john@doo.com",
+		"GIT_AUTHOR_EMAIL=john@doo.com",
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	require.NoError(t, cmd.Run())
 
 	cmd = exec.Command("git", "remote", "add", "origin", "git@github.com:org1/repo1.git")
