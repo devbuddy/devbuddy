20fc0513fbbe7bf3366c78c8c23a7ecab723df30
Pior Bastida <pior@pbastida.net>
2018-06-03 22:47:41 -0400

    Update with the zsh pull request

--- pkg/integration/zsh.go
+++ pkg/integration/zsh.go
@@ -1,8 +1,8 @@
 package integration
 
 var zshSource = `
-promptcmd() { $("__dad_prompt_command") }
-if [[ ! "${precmd_functions[@]}" == *__dad_prompt_command* ]]; then
+promptcmd() { $("__bud_prompt_command") }
+if [[ ! "${precmd_functions[@]}" == *__bud_prompt_command* ]]; then
   precmd_functions+=(promptcmd)
 fi
 `
