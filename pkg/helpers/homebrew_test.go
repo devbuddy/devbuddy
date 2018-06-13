1cbea86e5541f4cb7004072a9cedbfb40d3de614
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-06-12 21:55:41 -0400

    Adds more tests for caskroom

--- pkg/helpers/homebrew_test.go
+++ pkg/helpers/homebrew_test.go
@@ -15,10 +15,14 @@ func TestIsInCellar(t *testing.T) {
 
 	cellarPath := filepath.Join(prefix, "Cellar")
 
+	caskroomPath := filepath.Join(prefix, "Caskroom")
+
 	os.MkdirAll(filepath.Join(cellarPath, "curl", "1.2.3"), os.ModePerm)
+	os.MkdirAll(filepath.Join(caskroomPath, "emacs", "26.1"), os.ModePerm)
 
 	h := NewHomebrewWithPrefix(prefix)
 
-	require.Truef(t, h.IsInstalled("curl"), "Curl is missing from Cellar %s", cellarPath)
-	require.Falsef(t, h.IsInstalled("vim"), "Curl is missing from Cellar %s", cellarPath)
+	require.Truef(t, h.IsInstalled("curl"), "Curl is properly installed in Cellar %s", cellarPath)
+	require.Falsef(t, h.IsInstalled("vim"), "vim is missing from Homebrew %s", prefix)
+	require.True(t, h.IsInstalled("emacs"), "Emacs is properly installed in caskroom %s", caskroomPath)
 }
