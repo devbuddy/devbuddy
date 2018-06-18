adfad5418297f5c8c68f310b8a1d8c9928911781
Pior Bastida <pior@pbastida.net>
2018-06-17 23:32:33 -0400

    wip

--- tests/test_create.py
+++ tests/test_create.py
@@ -8,8 +8,8 @@ def test_create(cmd):
 
 def test_existing_project(cmd, project):
     output = cmd.run(f"bud create {project.org}/{project.name}")
-    assert "github.com:devbuddy_tests/poipoi" in output
+    assert "project already exists locally" in output
 
     output = cmd.run("pwd")
-    assert output.endswith("/src/github.com/devbuddy_tests/poipoi")
+    assert output.endswith(f"/src/github.com/{project.org}/{project.name}")
 
