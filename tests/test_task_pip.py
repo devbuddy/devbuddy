99052d10edea82a0acae292e204afc0c0a6542b1
Pior Bastida <pior@pbastida.net>
2018-06-18 21:18:04 -0400

    py3.6.5

--- tests/test_task_pip.py
+++ tests/test_task_pip.py
@@ -2,7 +2,7 @@
 def test_requirements(cmd, project):
     project.write_devyml("""
         up:
-        - python: 3.6.3
+        - python: 3.6.5
         - pip: [requirements.txt]
     """)
     project.write_file("requirements.txt", "test==2.3.4.5\n")
