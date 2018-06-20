680d4cae63d29005bce57906f546b90379d1114b
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-06-19 23:00:30 -0400

    Fix zsh shell integration

--- pkg/integration/zsh.go
+++ pkg/integration/zsh.go
@@ -1,7 +1,7 @@
 package integration
 
 var zshSource = `
-promptcmd() { $("__bud_prompt_command") }
+promptcmd() { $(__bud_prompt_command) }
 if [[ ! "${precmd_functions[@]}" == *__bud_prompt_command* ]]; then
   precmd_functions+=(promptcmd)
 fi
