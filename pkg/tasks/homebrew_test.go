e40c3f5041d278098eacc115ab5e5ea0189d81b7
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-06-19 20:51:37 -0400

    [FEATURE] Introduces homebrew task (#67)
    * Respond to code review and migrate to the new task api
    
    * Fix naming
    
    * Files to formulas
    
    * Simplify constructor for homebrew
    
    * Split the work for homebrew between cask and cellar
    
    * Fixes comments
    
    * Adds more tests for caskroom
    
    * Privatize getPathParts/setPathParts
    
    * Adds some explanation about formula construction
    
    * Privatize isInstalled of cellar and caskroom
    
    * Removes uncessary outputfilter
    
    * Moves codes that test installation in needed

---
+++ pkg/tasks/homebrew_test.go
@@ -0,0 +1,16 @@
+package tasks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHomebrew(t *testing.T) {
+	task := ensureLoadTestTask(t, `
+homebrew:
+  - file1
+  - file2
+`)
+	require.Equal(t, task.(*Homebrew).formulas, []string{"file1", "file2"})
+}
