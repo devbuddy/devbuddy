f0fe2157b43135d25d035edde57d0c7d75faf408
Pior Bastida <pior@pbastida.net>
2018-06-23 18:46:49 -0400

    Add a task to install a python project in develop mode

---
+++ pkg/helpers/store/store.go
@@ -0,0 +1,91 @@
+package store
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/devbuddy/devbuddy/pkg/utils"
+)
+
+var dirName = ".devbuddy"
+
+// Store is the place to record information or keep files about a project
+type Store struct {
+	projectPath string
+}
+
+// New returns an instance of Store
+func New(projectPath string) *Store {
+	return &Store{projectPath: projectPath}
+}
+
+func (s *Store) path() string {
+	return filepath.Join(s.projectPath, dirName)
+}
+
+func (s *Store) ensureInit() (err error) {
+	if !utils.PathExists(s.path()) {
+		err = os.MkdirAll(s.path(), 0755)
+		if err != nil {
+			return
+		}
+	}
+
+	gitignore := filepath.Join(s.path(), ".gitignore")
+	if !utils.PathExists(gitignore) {
+		err = ioutil.WriteFile(gitignore, []byte("*"), 0644)
+		if err != nil {
+			return
+		}
+	}
+	return nil
+}
+
+func (s *Store) stateFilePath(kind, key string) string {
+	return filepath.Join(s.path(), fmt.Sprintf("%s-%s", kind, key))
+}
+
+func makeKeyFromPath(path string) string {
+	return strings.Replace(path, string(filepath.Separator), "--", -1)
+}
+
+// RecordFileChange stores the modification time of a file.
+func (s *Store) RecordFileChange(path string) error {
+	err := s.ensureInit()
+	if err != nil {
+		return err
+	}
+
+	info, err := os.Stat(filepath.Join(s.projectPath, path))
+	if err != nil {
+		return err
+	}
+	mtime := info.ModTime()
+
+	stateFilePath := s.stateFilePath("mtime", makeKeyFromPath(path))
+	err = ioutil.WriteFile(stateFilePath, []byte{}, 0644)
+	if err != nil {
+		return err
+	}
+
+	return os.Chtimes(stateFilePath, mtime, mtime)
+}
+
+// HasFileChanged detects whether a path has changed since the last call to RecordFileChange().
+// Defaults to true if path doesn't exists or RecordFileChange() was never called.
+func (s *Store) HasFileChanged(path string) bool {
+	info, err := os.Stat(filepath.Join(s.projectPath, path))
+	if err != nil {
+		return true
+	}
+
+	stateInfo, err := os.Stat(s.stateFilePath("mtime", makeKeyFromPath(path)))
+	if err != nil {
+		return true
+	}
+
+	return info.ModTime().After(stateInfo.ModTime())
+}
---
+++ pkg/helpers/store/store_test.go
@@ -0,0 +1,61 @@
+package store
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Flaque/filet"
+)
+
+func touch(t *testing.T, path string) {
+	err := ioutil.WriteFile(path, []byte(""), 0644)
+	require.NoError(t, err)
+}
+
+func TestWithoutFile(t *testing.T) {
+	defer filet.CleanUp(t)
+	tmpdir := filet.TmpDir(t, "")
+	s := New(tmpdir)
+
+	require.True(t, s.HasFileChanged("testfile"))
+}
+
+func TestFirstTime(t *testing.T) {
+	defer filet.CleanUp(t)
+	tmpdir := filet.TmpDir(t, "")
+	s := New(tmpdir)
+
+	path := filepath.Join(tmpdir, "testfile")
+	touch(t, path)
+
+	require.True(t, s.HasFileChanged("testfile"))
+}
+
+func TestRecordWithoutFile(t *testing.T) {
+	defer filet.CleanUp(t)
+	tmpdir := filet.TmpDir(t, "")
+	s := New(tmpdir)
+
+	err := s.RecordFileChange("testfile")
+	require.Error(t, err)
+}
+
+func TestRecord(t *testing.T) {
+	defer filet.CleanUp(t)
+	tmpdir := filet.TmpDir(t, "")
+	s := New(tmpdir)
+
+	path := filepath.Join(tmpdir, "testfile")
+	touch(t, path)
+
+	err := s.RecordFileChange("testfile")
+	require.NoError(t, err)
+
+	require.False(t, s.HasFileChanged("testfile"))
+
+	touch(t, path)
+	require.True(t, s.HasFileChanged("testfile"))
+}
