632a69cea2efc549ad390eceecbee4339c9be551
Pior Bastida <pior@pbastida.net>
2018-10-07 20:39:40 -0400

    Fix issue with pipenv+pip-18.1

--- tests/test_task_pipfile.py
+++ tests/test_task_pipfile.py
@@ -3,6 +3,10 @@ def test_simple(cmd, project):
     project.write_devyml("""
         up:
         - python: 3.6.5
+        - custom:
+            name: force pip version - https://github.com/pypa/pipenv/issues/2924
+            met?: pip --disable-pip-version-check freeze --all | grep -q 'pip==18.0'
+            meet: pip --disable-pip-version-check install 'pip==18.0'
         - pipfile
     """)
     project.write_file("Pipfile", """[packages]\n"test" = "==2.3.4.5"\n""")
