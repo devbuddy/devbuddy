678454ebbdac9d53a67e3c29ad0f855d7ae122ae
Pior Bastida <pior@pbastida.net>
2018-10-08 17:58:42 -0400

    Check err returned by fmt.Fprintf() (#158)
    * Check err returned by fmt.Fprintf()
    
    * Update lint config after gas linter renamed as gosec

--- pkg/termui/base.go
+++ pkg/termui/base.go
@@ -2,6 +2,8 @@ package termui
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	color "github.com/logrusorgru/aurora"
@@ -15,6 +17,13 @@ type baseUI struct {
 func (u *baseUI) Debug(format string, params ...interface{}) {
 	if u.debugEnabled {
 		msg := fmt.Sprintf(format, params...)
-		fmt.Fprintf(u.out, "BUD_DEBUG: %s\n", color.Gray(msg))
+		Fprintf(u.out, "BUD_DEBUG: %s\n", color.Gray(msg))
+	}
+}
+
+func Fprintf(w io.Writer, format string, a ...interface{}) {
+	_, err := fmt.Fprintf(w, format, a...)
+	if err != nil {
+		log.Fatalf("failed to write to console: %s", err)
 	}
 }
