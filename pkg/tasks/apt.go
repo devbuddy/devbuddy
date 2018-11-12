b820e6364b34d7c50dd73d9be7bcc63262e6f16b
Pior Bastida <pior@pbastida.net>
2018-11-11 21:03:38 -0500

    use launcherror

--- pkg/tasks/apt.go
+++ pkg/tasks/apt.go
@@ -40,8 +40,8 @@ func (a *aptInstall) description() string {
 
 func (a *aptInstall) needed(ctx *context) (bool, error) {
 	result := commandSilent(ctx, "dpkg", "-s", a.packageName).Capture()
-	if result.Code == -1 {
-		return false, fmt.Errorf("failed to check if package is installed: %s", result.Error)
+	if result.LaunchError != nil {
+		return false, fmt.Errorf("failed to check if package is installed: %s", result.LaunchError)
 	}
 	return result.Code != 0, nil
 }
