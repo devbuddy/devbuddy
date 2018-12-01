993177110d4ec0f5aeba23140f3dd81d444beeab
Pior Bastida <pior@pbastida.net>
2018-12-01 16:54:26 -0500

    Improve interface and implementation of the store (#200)
    * Improve interface and implementation of the store
    
    * wip
    
    * wip
    
    * wip
    
    * empty key

--- pkg/helpers/store/store.go
+++ pkg/helpers/store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -26,7 +27,16 @@ func (s *Store) path() string {
 	return filepath.Join(s.projectPath, dirName)
 }
 
+func (s *Store) pathForKey(key string) string {
+	filePathForKey := strings.Replace(key, string(filepath.Separator), "--", -1)
+	return filepath.Join(s.path(), filePathForKey)
+}
+
 func (s *Store) ensureInit() (err error) {
+	if !utils.PathExists(s.projectPath) {
+		return fmt.Errorf("failed to initialize the store: project path does not exist: %s", s.projectPath)
+	}
+
 	if !utils.PathExists(s.path()) {
 		err = os.MkdirAll(s.path(), 0755)
 		if err != nil {
@@ -44,28 +54,61 @@ func (s *Store) ensureInit() (err error) {
 	return nil
 }
 
-func (s *Store) stateFilePath(kind, key string) string {
-	return filepath.Join(s.path(), fmt.Sprintf("%s-%s", kind, key))
-}
+// Set stores a byte slice for a key
+func (s *Store) Set(key string, value []byte) error {
+	if key == "" {
+		return errors.New("empty string is an invalid key")
+	}
 
-func makeKeyFromPath(path string) string {
-	return strings.Replace(path, string(filepath.Separator), "--", -1)
-}
-
-// RecordFileChange stores the modification time of a file.
-func (s *Store) RecordFileChange(path string) error {
 	err := s.ensureInit()
 	if err != nil {
 		return err
 	}
 
+	return ioutil.WriteFile(s.pathForKey(key), value, 0644)
+}
+
+// SetString stores a string for a key
+func (s *Store) SetString(key string, value string) error {
+	return s.Set(key, []byte(value))
+}
+
+// Get retrieves a byte slice for a key
+func (s *Store) Get(key string) ([]byte, error) {
+	if key == "" {
+		return nil, errors.New("empty string is an invalid key")
+	}
+
+	pathForKey := s.pathForKey(key)
+
+	if _, err := os.Stat(pathForKey); os.IsNotExist(err) {
+		return nil, nil
+	}
+
+	content, err := ioutil.ReadFile(pathForKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
+
+// GetString retrieves a string for a key
+func (s *Store) GetString(key string) (string, error) {
+	value, err := s.Get(key)
+	return string(value), err
+}
+
+// DEPRECATED: don't use the RecordFileChange/HasFileChanged methods, they will be removed.
+// It should not be part of the Store
+
+// RecordFileChange stores the modification time of a file.
+func (s *Store) RecordFileChange(path string) error {
 	checksum, err := utils.FileChecksum(filepath.Join(s.projectPath, path))
 	if err != nil {
 		return err
 	}
-
-	stateFilePath := s.stateFilePath("checksum", makeKeyFromPath(path))
-	return ioutil.WriteFile(stateFilePath, []byte(checksum), 0644)
+	return s.SetString(s.pathForKey("checksum-"+path), checksum)
 }
 
 // HasFileChanged detects whether a path has changed since the last call to RecordFileChange().
@@ -76,11 +119,10 @@ func (s *Store) HasFileChanged(path string) (bool, error) {
 		return true, nil
 	}
 
-	stateFilePath := s.stateFilePath("checksum", makeKeyFromPath(path))
-	content, err := ioutil.ReadFile(stateFilePath)
+	content, err := s.GetString(s.pathForKey("checksum-" + path))
 	if err != nil {
 		return true, nil
 	}
 
-	return checksum != string(content), nil
+	return checksum != content, nil
 }
