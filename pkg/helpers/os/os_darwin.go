ac76c0c7715628c1661f6feaac6824e751769c51
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-12-07 11:19:23 -0500

    Adds os detection (#213)
    * Adds os detection helper.
    
    * Improve api.
    
    * Adds os specific implementation.
    
    * Adds platform dependant tests.
    
    * Adds more tests.
    
    * Adds more tests.
    
    * Fixes broken darwin tests.
    
    * Adds more tests.
    
    * Responded to feedback.

---
+++ pkg/helpers/os/os_darwin.go
@@ -0,0 +1,60 @@
+package os
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+	"syscall"
+)
+
+// New returns an OS identifier.
+func New() (o *OS, err error) {
+	variant := "unknown"
+
+	if variant, err = syscall.Sysctl("kern.osrelease"); err != nil {
+		return nil, err
+	}
+
+	return &OS{runtime.GOOS, variant}, nil
+}
+
+// GetVariant returns the variant of the os identified by `runtime`.
+func (o *OS) GetVariant() (string, error) {
+	versiomNumberList := strings.Split(o.release, ":")
+	versiomNumber := versiomNumberList[len(versiomNumberList)-1]
+	versiomNumber = strings.TrimSpace(versiomNumber)
+
+	version := strings.Split(versiomNumber, ".")[0]
+
+	if version == "18" {
+		return "mojave", nil
+	} else if version == "17" {
+		return "high sierra", nil
+	} else if version == "16" {
+		return "sierra", nil
+	} else if version == "15" {
+		return "el capitan", nil
+	} else if version == "14" {
+		return "yosemite", nil
+	} else if version == "13" {
+		return "mavericks", nil
+	} else if version == "12" {
+		return "mountain lion", nil
+	} else if version == "11" {
+		return "lion", nil
+	} else if version == "10" {
+		return "snow leopard", nil
+	} else if version == "9" {
+		return "leopard", nil
+	} else if version == "8" {
+		return "tiger", nil
+	} else if version == "7" {
+		return "panther", nil
+	} else if version == "6" {
+		return "jaguar", nil
+	} else if version == "5" {
+		return "puma", nil
+	}
+
+	return "", fmt.Errorf("Cannot identify variant '%s' for darwin", version)
+}
