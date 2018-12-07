6ceaed7c8eb441cbc25ad637c3b259ed69703811
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2018-12-07 14:23:04 -0500

    Split tests in different func.

--- pkg/helpers/osidentity/osidentity_darwin_test.go
+++ pkg/helpers/osidentity/osidentity_darwin_test.go
@@ -9,5 +9,9 @@ import (
 func TestDetectIsMacOS(t *testing.T) {
 	i := Detect()
 	require.True(t, i.IsMacOS())
+}
+
+func TestDetectIsDebianLike(t *testing.T) {
+	i := Detect()
 	require.False(t, i.IsDebianLike())
 }
--- pkg/helpers/osidentity/osidentity_linux_test.go
+++ pkg/helpers/osidentity/osidentity_linux_test.go
@@ -9,5 +9,9 @@ import (
 func TestDetectIsDebianLike(t *testing.T) {
 	i := Detect()
 	require.True(t, i.IsDebianLike())
+}
+
+func TestDetectIsMacOS(t *testing.T) {
+	i := Detect()
 	require.False(t, i.IsMacOS())
 }
