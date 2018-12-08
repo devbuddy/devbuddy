2b95db8bf35222ae582e042737833b826fa66972
Pior Bastida <pior@pbastida.net>
2018-12-07 22:21:38 -0500

    Add support for setuptools extra in python_develop task

--- pkg/tasks/python_develop.go
+++ pkg/tasks/python_develop.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"fmt"
+	"strings"
 )
 
 func init() {
@@ -12,17 +13,19 @@ func init() {
 }
 
 func parserPythonDevelop(config *taskConfig, task *Task) error {
-	_, err := config.getListOfStringsPropertyDefault("extras", []string{})
+	extras, err := config.getListOfStringsPropertyDefault("extras", []string{})
 	if err != nil {
 		return err
 	}
 
-	// TODO(pior): implement the logic for the extras
+	pipTarget := "."
+	if len(extras) > 0 {
+		pipTarget = fmt.Sprintf(".[%s]", strings.Join(extras, ","))
+	}
+	pipArgs := []string{"install", "--require-virtualenv", "-e", pipTarget}
 
 	builder := actionBuilder("install python package in develop mode", func(ctx *Context) error {
-		result := command(ctx, "pip", "install", "--require-virtualenv", "-e", ".").
-			AddOutputFilter("already satisfied").Run()
-
+		result := command(ctx, "pip", pipArgs...).AddOutputFilter("already satisfied").Run()
 		if result.Error != nil {
 			return fmt.Errorf("Pip failed: %s", result.Error)
 		}
