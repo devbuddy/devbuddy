7eaa96249c27216a0f404173c3ac40af10f6607a
Pior Bastida <pior@pbastida.net>
2018-12-07 21:39:19 -0500

    wip

--- pkg/tasks/python_develop.go
+++ pkg/tasks/python_develop.go
@@ -12,6 +12,13 @@ func init() {
 }
 
 func parserPythonDevelop(config *taskConfig, task *Task) error {
+	_, err := config.getListOfStringsPropertyDefault("extras", []string{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(pior): implement the logic for the extras
+
 	builder := actionBuilder("install python package in develop mode", func(ctx *Context) error {
 		result := command(ctx, "pip", "install", "--require-virtualenv", "-e", ".").
 			AddOutputFilter("already satisfied").Run()
