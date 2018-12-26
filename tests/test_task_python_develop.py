8db1f310738f8a10f111b3efdbdde1604d8d9986
Pior Bastida <pior@pbastida.net>
2018-12-26 15:14:05 -0500

    add test

--- tests/test_task_python_develop.py
+++ tests/test_task_python_develop.py
@@ -2,13 +2,13 @@ import os
 import textwrap
 
 
-def make_setuppy(version):
-    return textwrap.dedent(f"""
+def make_setuppy(version=1):
+    return textwrap.dedent("""
         from setuptools import setup, find_packages
-        setup(name='devbuddy-test-pkg', version='{version}')
+        setup(name='devbuddy-test-pkg', version='%s', extras_require={'test': ['test==2.3.4.5']})
 
         open("sentinel", "w").write("")
-    """)
+    """) % version
 
 
 def test_with_modification(cmd, project):
@@ -53,3 +53,18 @@ def test_without_modification(cmd, project):
     cmd.run("bud up")
 
     assert not os.path.exists(sentinel_path)
+
+
+def test_with_extra(cmd, project):
+    project.write_devyml("""
+        up:
+        - python: 3.6.5
+        - python_develop:
+            extras: [test]
+    """)
+    project.write_file("setup.py", make_setuppy())
+
+    cmd.run("bud up")
+
+    output = cmd.run("pip freeze")
+    assert 'test==2.3.4.5' in output.splitlines(False)
