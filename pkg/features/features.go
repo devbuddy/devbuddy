2660c02668ce35477b87f2246f4b92b04390a2e6
Pior Bastida <pior@pbastida.net>
2018-12-29 17:21:20 -0500

    WIP

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -4,9 +4,10 @@ import (
 	"github.com/devbuddy/devbuddy/pkg/config"
 	"github.com/devbuddy/devbuddy/pkg/env"
 	"github.com/devbuddy/devbuddy/pkg/project"
+	"github.com/devbuddy/devbuddy/pkg/termui"
 )
 
-// Get returns a feature selected by the name argument
+// Get returns a feature selected by the name argument.
 func Get(name string) (*Feature, error) {
 	return globalRegister.get(name)
 }
@@ -14,8 +15,17 @@ func Get(name string) (*Feature, error) {
 type activateFunc func(string, *config.Config, *project.Project, *env.Env) (bool, error)
 type deactivateFunc func(string, *config.Config, *env.Env)
 
+// Feature is the implementation of an environment feature.
 type Feature struct {
 	Name       string
 	Activate   activateFunc
 	Deactivate deactivateFunc
+}
+
+// Sync activates / deactivates the features in the instance of env.Env.
+// When a feature is already active but unknown, it will be ignored completely.
+// When a param changes, the feature is deactivated with the current param then activated with the new param.
+func Sync(cfg *config.Config, proj *project.Project, ui *termui.UI, env *env.Env, features map[string]string) {
+	runner := &runner{cfg: cfg, proj: proj, ui: ui, env: env, reg: globalRegister}
+	runner.sync(features)
 }
