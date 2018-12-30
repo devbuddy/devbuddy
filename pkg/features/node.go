1ae14d86ac0f5508eb901c3b2a53769720d1bbd7
Pior Bastida <pior@pbastida.net>
2018-12-29 23:53:48 -0500

    Add a task for NodeJS (#225)
    Add support for NodeJS

---
+++ pkg/features/node.go
@@ -0,0 +1,26 @@
+package features
+
+import (
+	"github.com/devbuddy/devbuddy/pkg/config"
+	"github.com/devbuddy/devbuddy/pkg/env"
+	"github.com/devbuddy/devbuddy/pkg/helpers"
+	"github.com/devbuddy/devbuddy/pkg/project"
+)
+
+func init() {
+	register("node", nodeActivate, nodeDeactivate)
+}
+
+func nodeActivate(version string, cfg *config.Config, proj *project.Project, env *env.Env) (bool, error) {
+	node := helpers.NewNode(cfg, version)
+	if !node.Exists() {
+		return true, nil
+	}
+	env.PrependToPath(node.BinPath())
+	return false, nil
+}
+
+func nodeDeactivate(version string, cfg *config.Config, env *env.Env) {
+	node := helpers.NewNode(cfg, version)
+	env.RemoveFromPath(node.Path())
+}
