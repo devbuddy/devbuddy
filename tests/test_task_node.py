e2b587e81b5ccfd87bb2188acfc3b0d28e88ac27
Pior Bastida <pior@pbastida.net>
2019-01-01 19:27:50 -0500

    Add test on npm install

--- tests/test_task_node.py
+++ tests/test_task_node.py
@@ -12,3 +12,23 @@ def test_env(cmd, project):
     output = cmd.run("node -v")
     assert "v10.15.0" == output
 
+
+def test_npm_install(cmd, project):
+    project.write_devyml("""
+        up:
+        - node: '10.15.0'
+    """)
+
+    project.write_file("package.json", """
+        {
+            "dependencies": {
+                "testpackage": "1.0.0"
+            }
+        }
+    """)
+
+    cmd.run("bud up")
+
+    output = cmd.run("npm list")
+    assert "testpackage@1.0.0" in output
+
