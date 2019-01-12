55847a895e18cb73d811e4fcbb7c63e302539d47
Pior Bastida <pior@pbastida.net>
2019-01-11 23:16:48 -0500

    Move the execution code for a task action in task_runner (#264)

--- pkg/tasks/task_action.go
+++ pkg/tasks/task_action.go
@@ -25,35 +25,3 @@ func actionNeeded(message string, args ...interface{}) *ActionResult {
 func actionNotNeeded() *ActionResult {
 	return &ActionResult{Needed: false}
 }
-
-func runAction(ctx *Context, action TaskAction) error {
-	desc := action.Description()
-
-	result := action.Needed(ctx)
-	if result.Error != nil {
-		return fmt.Errorf("The task action (%s) failed to detect whether it need to run: %s", desc, result.Error)
-	}
-
-	if result.Needed {
-		if desc != "" {
-			ctx.ui.TaskActionHeader(desc)
-		}
-		ctx.ui.Debug("Reason: \"%s\"", result.Reason)
-
-		err := action.Run(ctx)
-		if err != nil {
-			return fmt.Errorf("The task action failed to run: %s", err)
-		}
-
-		result = action.Needed(ctx)
-		if result.Error != nil {
-			return fmt.Errorf("The task action failed to detect if it is resolved: %s", result.Error)
-		}
-
-		if result.Needed {
-			return fmt.Errorf("The task action did not produce the expected result: %s", result.Reason)
-		}
-	}
-
-	return nil
-}
