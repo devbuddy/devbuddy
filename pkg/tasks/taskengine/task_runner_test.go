adf4a87044ecb9addd02970df19d98995de963f5
Pior Bastida <pior@pbastida.net>
2019-01-12 20:25:57 -0500

    Re-organize files in taskengine package

--- pkg/tasks/taskengine/task_runner_test.go
+++ pkg/tasks/taskengine/task_runner_test.go
@@ -1,91 +1,13 @@
 package taskengine
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
 	"testing"
 
-	"github.com/devbuddy/devbuddy/pkg/config"
-	"github.com/devbuddy/devbuddy/pkg/env"
-	"github.com/devbuddy/devbuddy/pkg/features"
-	"github.com/devbuddy/devbuddy/pkg/project"
 	"github.com/devbuddy/devbuddy/pkg/tasks/taskapi"
-	"github.com/devbuddy/devbuddy/pkg/termui"
 
 	"github.com/stretchr/testify/require"
 )
-
-func dummyTask(name string) *taskapi.Task {
-	return &taskapi.Task{TaskDefinition: &taskapi.TaskDefinition{Name: name}}
-}
-
-type taskRunnerMock struct {
-	taskError error
-	tasks     []*taskapi.Task
-}
-
-func (r *taskRunnerMock) Run(ctx *taskapi.Context, task *taskapi.Task) error {
-	r.tasks = append(r.tasks, task)
-	return r.taskError
-}
-
-type taskSelectorMock struct {
-	should bool
-}
-
-func (s *taskSelectorMock) ShouldRun(ctx *taskapi.Context, task *taskapi.Task) (bool, error) {
-	return s.should, nil
-}
-
-type testingAction struct {
-	desc            string
-	neededResults   []*taskapi.ActionResult
-	neededCallCount int
-
-	runResult    error
-	runCallCount int
-}
-
-func (a *testingAction) Description() string {
-	return a.desc
-}
-
-func (a *testingAction) Needed(ctx *taskapi.Context) *taskapi.ActionResult {
-	result := a.neededResults[a.neededCallCount]
-	if result == nil {
-		panic("the task should not have been called")
-	}
-	a.neededCallCount++
-	return result
-}
-
-func (a *testingAction) Run(ctx *taskapi.Context) error {
-	a.runCallCount++
-	return a.runResult
-}
-
-func newTestingAction(desc string, resultBefore, resultAfter *taskapi.ActionResult, runResult error) *testingAction {
-	return &testingAction{
-		desc:          desc,
-		neededResults: []*taskapi.ActionResult{resultBefore, resultAfter},
-		runResult:     runResult,
-	}
-}
-
-func setupTaskTesting() (*taskapi.Context, *bytes.Buffer) {
-	buf, ui := termui.NewTesting(false)
-
-	ctx := &taskapi.Context{
-		Project:  project.NewFromPath("/src/myproject"),
-		UI:       ui,
-		Cfg:      config.NewTestConfig(),
-		Env:      env.New([]string{}),
-		Features: features.NewFeatureSet(),
-	}
-
-	return ctx, buf
-}
 
 func TestRunActionNeeded(t *testing.T) {
 	ctx, output := setupTaskTesting()
@@ -194,58 +116,4 @@ func TestTaskRunnerWithError(t *testing.T) {
 
 	require.Equal(t, 0, action2.neededCallCount)
 	require.Equal(t, 0, action2.runCallCount)
-}
-
-func TestRun(t *testing.T) {
-	ctx, _ := setupTaskTesting()
-	tasks := []*taskapi.Task{dummyTask("1"), dummyTask("2")}
-
-	taskRunner := &taskRunnerMock{}
-	taskSelector := &taskSelectorMock{true}
-
-	success, err := Run(ctx, taskRunner, taskSelector, tasks)
-	require.NoError(t, err)
-	require.True(t, success)
-
-	require.Equal(t, tasks, taskRunner.tasks)
-}
-
-func TestRunRequiredTaskCheck(t *testing.T) {
-	ctx, _ := setupTaskTesting()
-	tasks := []*taskapi.Task{
-		&taskapi.Task{TaskDefinition: &taskapi.TaskDefinition{Key: "pip", RequiredTask: "python"}},
-	}
-
-	success, err := Run(ctx, nil, nil, tasks)
-	require.EqualError(t, err, "You must specify a python task before a pip task")
-	require.False(t, success)
-}
-
-func TestRunWithTaskError(t *testing.T) {
-	ctx, _ := setupTaskTesting()
-	tasks := []*taskapi.Task{dummyTask("1"), dummyTask("2")}
-
-	taskRunner := &taskRunnerMock{taskError: fmt.Errorf("oops")}
-	taskSelector := &taskSelectorMock{true}
-
-	success, err := Run(ctx, taskRunner, taskSelector, tasks)
-	require.NoError(t, err)
-	require.False(t, success)
-
-	require.Equal(t, 1, len(taskRunner.tasks))
-	require.Equal(t, tasks[0], taskRunner.tasks[0])
-}
-
-func TestRunWithTaskWithOsRequirement(t *testing.T) {
-	ctx, _ := setupTaskTesting()
-	tasks := []*taskapi.Task{dummyTask("1"), dummyTask("2")}
-
-	taskRunner := &taskRunnerMock{taskError: fmt.Errorf("oops")}
-	taskSelector := &taskSelectorMock{false}
-
-	success, err := Run(ctx, taskRunner, taskSelector, tasks)
-	require.NoError(t, err)
-	require.True(t, success)
-
-	require.Equal(t, 0, len(taskRunner.tasks))
 }
