fc5e851e367d766a73c0e1a274a5a9f4b6e225c2
Mathieu Leduc-Hamel <info@mlhamel.org>
2019-01-15 01:23:43 +0000

    Removes executor builder for now.

--- pkg/executor/executor_builder.go
+++
@@ -1,7 +0,0 @@
-package executor
-
-// ExecutorBuilder build executor with specific program and args
-type ExecutorBuilder interface {
-	NewExecutor(program string, args ...string) Executor
-	NewShell(cmdline string) Executor
-}
