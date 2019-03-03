7ba5ad9cf9cfbcac235ba8057fca66885b0f200b
Pior Bastida <pior@pbastida.net>
2019-03-03 14:26:16 -0500

    Add support for node packages with NPM (#242)
    * Add a taskAction to install node packages with npm
    
    * Add test on npm install
    
    * fix

--- pkg/tasks/node.go
+++ pkg/tasks/node.go
@@ -4,6 +4,7 @@ import (
 	"github.com/devbuddy/devbuddy/pkg/context"
 	"github.com/devbuddy/devbuddy/pkg/helpers"
 	"github.com/devbuddy/devbuddy/pkg/tasks/taskapi"
+	"github.com/devbuddy/devbuddy/pkg/utils"
 )
 
 func init() {
@@ -30,6 +31,16 @@ func parseNode(config *taskapi.TaskConfig, task *taskapi.Task) error {
 	task.AddActionWithBuilder("install nodejs from https://nodejs.org", run).
 		OnFunc(condition).
 		SetFeature("node", version)
+
+	npmInstall := func(ctx *context.Context) error {
+		if !utils.PathExists("package.json") {
+			ctx.UI.TaskWarning("No package.json found.")
+			return nil
+		}
+		return command(ctx, "npm", "install", "--no-progress").Run().Error
+	}
+	task.AddActionWithBuilder("install dependencies with NPM", npmInstall).
+		OnFileChange("package.json")
 
 	return nil
 }
