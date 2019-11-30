471b1fc98f24fb931e7f8e7dad51e9eaa843a6a7
Pior Bastida <pior@pbastida.net>
2019-11-30 11:00:44 -0500

    [FEATURE] Add basic auto-completion support for ZSH

---
+++ pkg/integration/shells.go
@@ -0,0 +1,33 @@
+package integration
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+type ShellIdentity string
+
+var (
+	BASH ShellIdentity = "bash"
+	ZSH  ShellIdentity = "zsh"
+)
+
+func DetectShell() (ShellIdentity, error) {
+	proc, err := ps.FindProcess(os.Getppid())
+	if err != nil {
+		return "", fmt.Errorf("failed to get parent process info: %s", err)
+	}
+	parentProcessPath := proc.Executable()
+
+	switch {
+	case strings.HasSuffix(parentProcessPath, "bash"):
+		return BASH, nil
+	case strings.HasSuffix(parentProcessPath, "zsh"):
+		return ZSH, nil
+	}
+
+	return "", fmt.Errorf("parent process is not a supported shell: %s", parentProcessPath)
+}
