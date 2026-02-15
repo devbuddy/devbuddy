9ed2787410fbdc2c18f5a066b62ab69d51ff6f1c
Pior Bastida <pior@pbastida.net>
2026-02-15 16:36:47 +0100

    Remove legacy GOPATH mode from Go task

--- tests/task_go_test.go
+++ tests/task_go_test.go
@@ -25,11 +25,7 @@ func Test_Task_Go(t *testing.T) {
 	// Use sub-tests to avoid downloading the go runtime multiple times.
 	c := CreateContextAndInit(t)
 
-	t.Run("gomod_with_gopath", func(t *testing.T) {
-		// Most common configuration, Modules and GOPATH set.
-
-		c.Run(t, "export GOPATH=~")
-
+	t.Run("installs_and_runs_go_modules", func(t *testing.T) {
 		p := CreateProject(t, c,
 			`up:`,
 			`- go:`,
@@ -44,54 +40,14 @@ func Test_Task_Go(t *testing.T) {
 		lines = c.Run(t, "go version")
 		OutputContains(t, lines, "go version go1.23.6")
 
-		lines = c.Run(t, "echo GO111MODULE=${GO111MODULE}#")
-		OutputContains(t, lines, "GO111MODULE=#")
-
-		c.Write(t, "main.go", testingGoMain) // depends on github.com/devbuddy/tiny-test-go-package
-		c.Run(t, "go mod init")
-		c.Run(t, "go mod tidy", context.Timeout(15*time.Second))
-		lines = c.Run(t, "go run main.go", context.Timeout(time.Minute))
-		OutputContains(t, lines, "Is it working: true")
-	})
-
-	t.Run("gopath_absent", func(t *testing.T) {
-		// Less common configuration, Modules but no GOPATH set.
-
-		c.Run(t, "unset GOPATH")
-
-		p := CreateProject(t, c,
-			`up:`,
-			`- go:`,
-			`    version: "1.23.6"`,
-			`    modules: true`,
-		)
-		lines := c.Cd(t, p.Path)
-		OutputContains(t, lines, "activated: golang 1.23.6+mod")
-
-		lines = c.Run(t, "bud up", context.Timeout(2*time.Minute))
-		OutputContains(t, lines, "◼︎ Golang (1.23.6)")
-
-		lines = c.Run(t, "go version")
-		OutputContains(t, lines, "go version go1.23.6")
-
-		lines = c.Run(t, "echo GO111MODULE=${GO111MODULE}#")
-		OutputContains(t, lines, "GO111MODULE=on#")
-
 		c.Write(t, "main.go", testingGoMain)
-		c.Run(t, "go mod init project2")
+		c.Run(t, "go mod init github.com/orgname/project", context.Timeout(15*time.Second))
 		c.Run(t, "go mod tidy", context.Timeout(15*time.Second))
 		lines = c.Run(t, "go run main.go", context.Timeout(time.Minute))
 		OutputContains(t, lines, "Is it working: true")
 	})
 
-	t.Run("legacy_gopath_mode", func(t *testing.T) {
-		// Old configuration: Modules disabled.
-		// This is named "Legacy Go path mode" in https://go.dev/ref/mod#mod-commands
-		// Note: GOPATH-mode `go get` was removed in Go 1.22, so we only verify
-		// that bud configures the environment correctly.
-
-		c.Run(t, "export GOPATH=~")
-
+	t.Run("modules_false_is_rejected", func(t *testing.T) {
 		p := CreateProject(t, c,
 			`up:`,
 			`- go:`,
@@ -100,13 +56,7 @@ func Test_Task_Go(t *testing.T) {
 		)
 		c.Cd(t, p.Path)
 
-		lines := c.Run(t, "bud up", context.Timeout(2*time.Minute))
-		OutputContains(t, lines, "◼︎ Golang (1.23.6)")
-
-		lines = c.Run(t, "go version")
-		OutputContains(t, lines, "go version go1.23.6")
-
-		lines = c.Run(t, "echo GO111MODULE=${GO111MODULE}#")
-		OutputContains(t, lines, "GO111MODULE=off#")
+		lines := c.Run(t, "bud up", context.ExitCode(1), context.Timeout(2*time.Minute))
+		OutputContains(t, lines, `task "go": "modules: false" is no longer supported for task "go"`)
 	})
 }
