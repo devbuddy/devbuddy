f68c7e75a936b261dd584d6fedd50c9b9be7269d
Pior Bastida <pior@pbastida.net>
2026-05-15 12:17:03 +0200

    Add fast CLI test harness (#543)

--- tests/fast/harness_test.go
+++ tests/fast/harness_test.go
@@ -2,41 +2,51 @@ package integration
 
 import (
 	"testing"
+	"time"
 
-	"github.com/devbuddy/devbuddy/tests/context"
-	"github.com/devbuddy/devbuddy/tests/internal/harness"
+	"github.com/devbuddy/devbuddy/tests/internal/cliharness"
 )
 
-type Project = harness.Project
+type Context = cliharness.Context
+type Project = cliharness.Project
+type RunOption = cliharness.RunOption
 
 func TestMain(m *testing.M) {
-	harness.TestMain(m)
+	cliharness.TestMain(m)
 }
 
-func CreateContext(t *testing.T) *context.TestContext {
-	return harness.CreateContext(t)
+func CreateContext(t *testing.T) *cliharness.Context {
+	return cliharness.CreateContext(t)
 }
 
-func CreateContextAndInit(t *testing.T) *context.TestContext {
-	return harness.CreateContextAndInit(t)
+func CreateContextAndInit(t *testing.T) *cliharness.Context {
+	return cliharness.CreateContextAndInit(t)
 }
 
-func CreateContextAndProject(t *testing.T, devYmlLines ...string) (*context.TestContext, Project) {
-	return harness.CreateContextAndProject(t, devYmlLines...)
+func CreateContextAndProject(t *testing.T, devYmlLines ...string) (*cliharness.Context, Project) {
+	return cliharness.CreateContextAndProject(t, devYmlLines...)
 }
 
-func CreateProject(t *testing.T, c *context.TestContext, devYmlLines ...string) Project {
-	return harness.CreateProject(t, c, devYmlLines...)
+func CreateProject(t *testing.T, c *cliharness.Context, devYmlLines ...string) Project {
+	return cliharness.CreateProject(t, c, devYmlLines...)
+}
+
+func ExitCode(code int) RunOption {
+	return cliharness.ExitCode(code)
+}
+
+func Timeout(timeout time.Duration) RunOption {
+	return cliharness.Timeout(timeout)
 }
 
 func OutputContains(t *testing.T, lines []string, subStrings ...string) {
-	harness.OutputContains(t, lines, subStrings...)
+	cliharness.OutputContains(t, lines, subStrings...)
 }
 
 func OutputNotContains(t *testing.T, lines []string, subStrings ...string) {
-	harness.OutputNotContains(t, lines, subStrings...)
+	cliharness.OutputNotContains(t, lines, subStrings...)
 }
 
 func OutputEqual(t *testing.T, lines []string, expectedLines ...string) {
-	harness.OutputEqual(t, lines, expectedLines...)
+	cliharness.OutputEqual(t, lines, expectedLines...)
 }
