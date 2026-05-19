d64671e368b0d40404dbec6dbf73204e4c6b1c55
Pior Bastida <pior@pbastida.net>
2026-05-19 05:49:34 +0200

    Use stdlib venv for Python environments

--- pkg/tasks/python_test.go
+++ pkg/tasks/python_test.go
@@ -9,9 +9,12 @@ import (
 func TestPythonOk(t *testing.T) {
 	task := ensureLoadTestTask(t, `python: 3.6.3`)
 
-	require.Equal(t, "Task Python (3.6.3) feature=python:3.6.3 actions=4", task.Describe())
+	require.Equal(t, "Task Python (3.6.3) feature=python:3.6.3 actions=3", task.Describe())
 	require.Equal(t, "3.6.3", task.Info)
-	require.Equal(t, 4, len(task.Actions))
+	require.Equal(t, 3, len(task.Actions))
+	require.Equal(t, "install PyEnv", task.Actions[0].Description())
+	require.Equal(t, "install Python version with PyEnv", task.Actions[1].Description())
+	require.Equal(t, "create virtualenv", task.Actions[2].Description())
 	requireFeature(t, task, "python", "3.6.3")
 }
 
